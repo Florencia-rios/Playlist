0f7178a277a650d4b15611cf76559a57fd0266a2
Florencia Rios <florencia.rios@etermax.com>
2024-02-06 10:52:02 -0300

    Borre los test

--- src/main/java/com/music/playlist/repository/PlaylistRepository.java
+++ src/main/java/com/music/playlist/repository/PlaylistRepository.java
@@ -2,8 +2,11 @@ package com.music.playlist.repository;
 
 import com.music.playlist.entity.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+//    @Query("Select * from Playlist where playlist.id := id ")
+//    String ss(String id);
 }
